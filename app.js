31fc668bbcc8e7c8a57c9769fb4699eb3b5ec79f
DS <aboussoubel@gmail.com>
2026-05-13 20:52:52 +0700

    Add ejs templates, form for sending messages

--- app.js
+++ app.js
@@ -5,17 +5,36 @@ const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 const app = express();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(import.meta.dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Index page");
+  res.render("index", { messages: messages });
 });
 
 app.get("/new", (req, res) => {
-    res.send("New message page");
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  messages.push({ text: req.body.text, user: req.body.user, added: new Date() });
+  res.render("index", { messages: messages });
 });
 
 app.listen(PORT, HOST, (error) => {
